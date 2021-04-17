6d1b3a9f0417c0ebf927660514c684602823ca54
Anand Raj <anandrajanand720@gmail.com>
2021-04-17 17:03:15 +0530

    Updates

---
+++ precache-manifest.491089da144befe28ad681672298f61a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30d7f999efea51a5b43438894f91a64",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "99229526b2a2c68e9197",
+    "url": "/todoapp/static/css/main.17f7c9c0.chunk.css"
+  },
+  {
+    "revision": "783d7b81fec503a43022",
+    "url": "/todoapp/static/js/2.a56fdfc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.a56fdfc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99229526b2a2c68e9197",
+    "url": "/todoapp/static/js/main.e5b07d6a.chunk.js"
+  },
+  {
+    "revision": "70af1543a0d8ef3d3687",
+    "url": "/todoapp/static/js/runtime-main.52cbb973.js"
+  }
+]);
